800a7c831f407b4bf475c17e4ca85cd8fe6c4bad
northphubadee <phubadee@dollarsmart.co>
2019-02-15 10:39:27 +0700

    w8

---
+++ index.js
@@ -0,0 +1,21 @@
+const { request } = require('graphql-request')
+
+async function main() {
+  const endpoint = 'https://api.graph.cool/simple/v1/cixos23120m0n0173veiiwrjr'
+
+  const query = /* GraphQL */ `
+    {
+      Movie(title: "Inception") {
+        releaseDate
+        actors {
+          name
+        }
+      }
+    }
+  `
+
+  const data = await request(endpoint, query)
+  console.log(JSON.stringify(data, undefined, 2))
+}
+
+main().catch(error => console.error(error))
